f5645dc6c5e0f740cd518904b2d885e99af5568b
Taofeek Olalere <olaleretaofeek@live.com>
2022-09-15 21:53:02 +0100

    Reduce N+1 query on users

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,7 +7,7 @@ module Types
     field :users, [Types::UserType], null: false
 
     def users
-      User.all
+      User.preload(:posts)
     end
 
     field :user, Types::UserType, null: false do

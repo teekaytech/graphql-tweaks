0f9f7f374700c746b2738300463e2b3ec2fec312
Taofeek Olalere <olaleretaofeek@live.com>
2022-09-15 20:06:05 +0100

    Add apollo client and other related libraries to frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './tailwind.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const link = createHttpLink({
+  uri: 'http://localhost:3000/graphql',
+})
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache()
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
